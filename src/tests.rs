04ef3dc4f77a9dfd50a1b02af5661123aa1b9794
ggbits <ggbits@bitbybit.gg>
2026-04-17 17:08:59 -0700

    Added more tests checking for len and invalid char in Hex and B64 from str.

--- src/tests.rs
+++ src/tests.rs
@@ -1,4 +1,6 @@
-use crate::types::{Base64, Bytes, Hex};
+use std::{any::Any, str::FromStr};
+
+use crate::types::{Base64, Bytes, DecodeError, Hex};
 
 #[test]
 fn test_bin_to_hex() {
@@ -25,7 +27,8 @@ fn test_hex_to_bin() {
 
 #[test]
 fn test_b64_to_bin() {
-    let b64_str = Base64("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string());
+    let b64_str =
+        Base64("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string());
     let bin_answer = Bytes(b"I'm killing your brain like a poisonous mushroom".to_vec());
 
     let result = Bytes::try_from(b64_str);
@@ -39,5 +42,45 @@ fn test_bin_to_b64() {
     assert_eq!(
         Base64("SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb29t".to_string()),
         Base64::from(bin_str)
+    )
+}
+
+#[test]
+fn test_b64_invalid_char() {
+    let str = "I'm killing your brain like a poisonous mushroom";
+
+    assert_eq!(
+        DecodeError("".to_string()).type_id(),
+        Base64::from_str(str).unwrap_err().type_id()
+    )
+}
+
+#[test]
+fn test_hex_invalid_char() {
+    let str = "02b4f3dj91256a";
+
+    assert_eq!(
+        DecodeError("".to_string()).type_id(),
+        Hex::from_str(str).unwrap_err().type_id()
+    )
+}
+
+#[test]
+fn test_b64_invalid_len() {
+    let str = "SSdtIGtpbGxpbmcgeW91ciBicmFpbiBsaWtlIGEgcG9pc29ub3VzIG11c2hyb";
+
+    assert_eq!(
+        DecodeError("".to_string()).type_id(),
+        Base64::from_str(str).unwrap_err().type_id()
+    )
+}
+
+#[test]
+fn test_hex_invalid_len() {
+    let str = "02b4f3d9125";
+
+    assert_eq!(
+        DecodeError("".to_string()).type_id(),
+        Base64::from_str(str).unwrap_err().type_id()
     )
 }
